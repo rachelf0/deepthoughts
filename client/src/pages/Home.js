c91098cb619cf583db2a980eac819d43bf3dbdbc
Rachel Fritz <rachelfritz0@gmail.com>
2020-10-20 13:08:36 -0500

    updates

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,27 +1,50 @@
 import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
-import { QUERY_THOUGHTS } from '../utils/queries';
+import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
+import Auth from '../utils/auth';
+
 import ThoughtList from '../components/ThoughtList';
+import FriendList from '../components/FriendList';
+import ThoughtForm from '../components/ThoughtForm';
 
 const Home = () => {
   // use useQuery hook to make query request
   const { loading, data } = useQuery(QUERY_THOUGHTS);
+  const { data: userData } = useQuery(QUERY_ME_BASIC);
+
   const thoughts = data?.thoughts || [];
   console.log(thoughts);
+
+  const loggedIn = Auth.loggedIn();
+  console.log("loggedIn: ", loggedIn);
+  console.log("userData: ", userData);
 
   return (
     <main>
       <div className="flex-row justify-space-between">
-        <div className="col-12 mb-3">
+        {loggedIn && (
+          <div className="col-12 mb-3">
+            <ThoughtForm />
+          </div>
+        )}
+        <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
           {loading ? (
             <div>Loading...</div>
           ) : (
-        <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
-      )}
-    </div>
-  </div>
-</main>
+              <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
+            )}
+        </div>
+      </div>
+      {loggedIn && userData ? (
+        <div className="col-12 col-lg-3 mb-3">
+          <FriendList
+            username={userData.me.username}
+            friendCount={userData.me.friendCount}
+            friends={userData.me.friends}
+          />
+        </div>
+      ) : null}
+    </main>
   );
 };
-
 export default Home;
